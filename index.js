f9e35a9fbe1bb834035143e06302e7e4c314e8da
gopalpaul1 <gopal.kumar.paul1@gmail.com>
2021-02-08 21:30:19 +0600

    Hungry Monster

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+
+//... Search For Food Names ...
+
 const searchFoods = () =>{
+
     const searchName = document.getElementById("searchArea").value;
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchName}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayFoods(data.meals))
+
 }
+
+//... All Foods ...
 
 const displayFoods = foods =>{
 
-
     const foodContainer = document.getElementById("foodContainer");
+
     foods.forEach(food => {
 
         const foodDiv = document.createElement("div");
         foodDiv.className = "foodContent";
+
         const foodInfo = `
 
             <div onclick="displayFood('${food.idMeal}')">
@@ -30,6 +38,7 @@ const displayFoods = foods =>{
 }
 
 const displayFood = (idMeal) =>{
+
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
     fetch(url)
     .then(res => res.json())
@@ -37,13 +46,17 @@ const displayFood = (idMeal) =>{
      
 }
 
+//... Foods Details ...
+
 const display = meals => {
 
     const foodDetail = document.getElementById("foodDetail");
 
         const foodsDiv = document.createElement("div");
         foodsDiv.className = "foodContent";
+
         const foodsInfo = `
+
             <div class="detailInfo">
                 <img class="image" src="${meals.strMealThumb}">
                 <h2 class="textAlign">${meals.strMeal}</h2>
@@ -55,9 +68,9 @@ const display = meals => {
                     <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient3}</p>
                     <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient4}</p>
                     <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient5}</p>
-                </div>    
-                    
+                </div>        
             </div>
+
         `
         foodsDiv.innerHTML = foodsInfo;
         foodDetail.appendChild(foodsDiv);
