39723d0f09878fd009cae75582ec82de497770da
gopalpaul1 <gopal.kumar.paul1@gmail.com>
2021-02-08 21:19:35 +0600

    Hungry Monster

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const searchFoods = () =>{
 
 const displayFoods = foods =>{
 
-    const foodContainer = document.getElementById("foodContainer");
 
+    const foodContainer = document.getElementById("foodContainer");
     foods.forEach(food => {
 
         const foodDiv = document.createElement("div");
         foodDiv.className = "foodContent";
         const foodInfo = `
 
-            <div onclick="displayFood('${food.strMeal}')">
+            <div onclick="displayFood('${food.idMeal}')">
                 <img class="image" src="${food.strMealThumb}">
                 <h3 class="textAlign">${food.strMeal}</h3>
             </div>
@@ -29,13 +29,41 @@ const displayFoods = foods =>{
 
 }
 
-const displayFood = strMeal =>{
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${strMeal}`
+const displayFood = (idMeal) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => display(data.meals[0]))
      
 }
+
+const display = meals => {
+
+    const foodDetail = document.getElementById("foodDetail");
+
+        const foodsDiv = document.createElement("div");
+        foodsDiv.className = "foodContent";
+        const foodsInfo = `
+            <div class="detailInfo">
+                <img class="image" src="${meals.strMealThumb}">
+                <h2 class="textAlign">${meals.strMeal}</h2>
+                <div class="ingredient">
+                    <h3>${meals.strArea}</h3>
+                    <h3>Ingredient:</h3>
+                    <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient1}</p>
+                    <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient2}</p>
+                    <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient3}</p>
+                    <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient4}</p>
+                    <p><i class="fa fa-bars" aria-hidden="true"></i> ${meals.strIngredient5}</p>
+                </div>    
+                    
+            </div>
+        `
+        foodsDiv.innerHTML = foodsInfo;
+        foodDetail.appendChild(foodsDiv);
+
+}
+
 
 
 
